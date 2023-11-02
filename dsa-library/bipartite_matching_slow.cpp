9663a0505a3b20ab97703269af1bbe0736e9cf24
Martin Madzin <madzin.m@gmail.com>
2023-11-02 16:58:53 +0100

    Bipartite matching modified.

--- dsa-library/bipartite_matching_slow.cpp
+++ dsa-library/bipartite_matching_slow.cpp
@@ -12,46 +12,48 @@ using pll = pair<ll, ll>;
 #define sz(x) (int)x.size()
 #define contains(s,x) ((s).find(x) != (s).end())
 
-int A, B, M;
-vector<vector<int>> g;
-
-bool dfs(int b, vector<bool> &visited, vector<int> &matching) {
+bool dfs(int b, vector<int> &matching, vector<bool> &visited, vector<vector<int>> &g) {
         visited[b] = true;
         if (matching[b] == -1) return true;
-        int a = matching[b];
-        for (int next_b : g[a]) {
-                if (!visited[next_b] && dfs(next_b, visited, matching)) {
-                        matching[next_b] = a;
+        for (int next_b : g[matching[b]]) {
+                if (!visited[next_b] && dfs(next_b, matching, visited, g)) {
+                        matching[next_b] = matching[b];
                         return true;
                 }
         }
         return false;
 }
 
-/* Finds maximum bipartite matching. Returns vector with the matching, where
-i-th element is paired with v[i] (v[i] == -1 if not paired). O(N * M) */
-vector<int> max_bipartite_matching() {
-        vector<int> matching(B, -1);
+/* Given bipartite graph `g` and it's one part `left`, represented as array of vertices,
+returns maximum bipartite matching in that graph as vector of paired verices,
+where first vertex in every pair is from `left` part. Time complexity: O(N * M). */
+vector<pii> max_bipartite_matching(vector<vector<int>> &g, vector<int> &left) {
+        vector<bool> matched(sz(g), false);
+        vector<int> matching(sz(g), -1);
         // this cycle runs min(A, B) times at maximum, because in every
         // iteration we increment matching by 1 and maximum matching size
         // is min(A, B)
         while (true) {
-                unordered_set<int> matched_as(all(matching));
-                vector<bool> visited(B, false);
                 bool found = false;
+                vector<bool> visited(sz(g), false);
                 // if alternating path which uses current a exists
-                for (int a = 0; a < A && !found; a++) if (!contains(matched_as, a)) {
-                        // try all possible alternating paths begginings
-                        for (int b : g[a]) {
+                for (int a : left) if (!matched[a]) {
+                        for (int b : g[a]) if (!visited[b]) {
+                                found = dfs(b, matching, visited, g);
                                 // improving path with a -> b beggining exists
-                                if (dfs(b, visited, matching)) {
+                                if (found) {
                                         matching[b] = a;
-                                        found = true;
                                         break;
                                 }
                         }
+                        if (found) break;
                 }
                 // no improving path exists so matching is the best possible
-                if(!found) return matching;
+                if (!found) break;
+                for (int a : matching) if (a != -1) matched[a] = true;
         }
+        // construct matching as array of pairs of paired vertices
+        vector<pii> answer;
+        for (int i = 0; i < sz(matching); i++) if (matching[i] != -1) answer.pb({ matching[i], i });
+        return answer;
 }
